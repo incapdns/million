b6a36243465009df33a72b596bc9a4c4e78b6cb6
Lucas <lucas@easy-way.app>
2025-12-16 10:34:59 -0300

    fix all

--- packages/react/execute.ts
+++ packages/react/execute.ts
@@ -1,9 +1,19 @@
 import { EXEC_KEY } from '../million/constants';
 
-export const execute = (fn: any, ...args: any[]) => {
-  return {
+export const execute =
+  <Args extends any[], R>(fn: (...args: Args) => R, ...args: Args): R =>
+    ({
+      [EXEC_KEY]: true,
+      fn,
+      args
+    }) as any
+
+export const bind = <Bound extends any[], Rest extends any[], R>(
+  fn: (...args: [...Bound, ...Rest]) => R,
+  ...bound: Bound
+): ((...rest: Rest) => R) =>
+  ({
     [EXEC_KEY]: true,
-    fn,
-    args
-  };
-};
+    fn: (...resolvedArgs: Bound) => fn.bind(undefined, ...resolvedArgs),
+    args: bound
+  }) as any
