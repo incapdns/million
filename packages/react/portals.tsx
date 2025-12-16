52c96b54158a659a955c4149b21817fb194a143a
Lucas <lucas@easy-way.app>
2025-12-15 23:00:46 -0300

    fix all

--- packages/react/portals.tsx
+++ packages/react/portals.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const RenderPortals = ({ portals }: { portals: any[] }) => {
   // Retorna os elementos React que foram empurrados no array
   return <>{portals}</>; 
